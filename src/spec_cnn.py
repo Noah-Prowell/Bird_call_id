0ccf958a09e559f1f22d6cfde7999a3b5d67b1f2
Noah Prowell <noahprowell@pop-os.localdomain>
2020-12-02 15:09:20 -0700

    adding six

--- src/spec_cnn.py
+++ src/spec_cnn.py
@@ -10,9 +10,9 @@ import numpy as np
 import PIL
 
 
-train = tf.keras.preprocessing.image_dataset_from_directory('train_imgs', labels = 'inferred', validation_split = .2, subset = 'training',
+train = tf.keras.preprocessing.image_dataset_from_directory('train_imgs/five_test', labels = 'inferred', validation_split = .2, subset = 'training',
                                                             image_size=(128,128), batch_size=32, seed = 42)
-test = tf.keras.preprocessing.image_dataset_from_directory('train_imgs', labels = 'inferred', validation_split = .2, subset = 'validation',
+test = tf.keras.preprocessing.image_dataset_from_directory('train_imgs/five_test', labels = 'inferred', validation_split = .2, subset = 'validation',
                                                             image_size=(128,128), batch_size=32, seed = 42,shuffle=False)
 
 
@@ -77,24 +77,22 @@ model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentro
 # model.add(Dense(264, activation='softmax'))
 # model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 print(model.summary())
-# epochs=10000
-# checkpoint_cb = keras.callbacks.ModelCheckpoint('fifth_model.h5', save_best_only= True)
-# early_stopping_cb = keras.callbacks.EarlyStopping(patience=10)
-# tensorboard_cb = keras.callbacks.TensorBoard()
+epochs=10000
+checkpoint_cb = keras.callbacks.ModelCheckpoint('fifth_model.h5', save_best_only= True)
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=10)
+tensorboard_cb = keras.callbacks.TensorBoard()
 
-# history1 = model.fit(
-#   train,
-#   validation_data=test,
-#   epochs=150, 
-#   batch_size= 32,
-#   callbacks=[checkpoint_cb, tensorboard_cb]
-# )
-# history2 = model.fit(
-#   train,
-#   validation_data=test,
-#   epochs=epochs, 
-#   batch_size= 32,
-#   callbacks=[checkpoint_cb, early_stopping_cb, tensorboard_cb]
-#   validation_steps=4274//32,
-#   steps_per_epoch=17099//32
-# )
+history1 = model.fit(
+  train,
+  validation_data=test,
+  epochs=150, 
+  batch_size= 32,
+  callbacks=[checkpoint_cb, tensorboard_cb]
+)
+history2 = model.fit(
+  train,
+  validation_data=test,
+  epochs=epochs, 
+  batch_size= 32,
+  callbacks=[checkpoint_cb, early_stopping_cb, tensorboard_cb]
+)
