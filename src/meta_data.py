9a7bbfa4629166338a3f8870016ef047d8c93f37
Noah Prowell <noahprowell@pop-os.localdomain>
2020-12-02 13:46:36 -0700

    fifth model

--- src/meta_data.py
+++ src/meta_data.py
@@ -6,15 +6,33 @@ df = pd.read_csv('data/train.csv')
 
 # columns to use: pitch, speed, latitude, longitude, elevation, volume
 
-X = df[['pitch', 'speed', 'latitude', 'longitude', 'elevation', 'volume']]
+X = df[['pitch', 'latitude', 'longitude', 'elevation', 'volume']]
 y = df.pop('species')
 
-conv_dict = {
-    'latitude': float,
-    'longitude': float, 
-    'elevation': int
-}
-
+X['elevation'] = [num.replace(' m', '') for num in X['elevation']]
+X['elevation'] = [num.replace('?', '') for num in X['elevation']]
+X['elevation'] = [num.replace('Unknown', '') for num in X['elevation']]
+X['elevation'] = [num.replace(',', '') for num in X['elevation']]
+X['elevation'] = [num.replace('~', '') for num in X['elevation']]
+conv = lambda i : i or None
+X['elevation'] = [conv(i) for i in X['elevation']] 
 X['pitch'] = X['pitch'].astype('category')
-X['speed'] = X['speed'].astype('category')
+X['volume'] = X['volume'].astype('category')
 
+X.dropna(inplace = True)
+# X['latitude'] = ast.literal_eval(X['latitude'])
+# X['longitude'] = X['longitude'].astype(float)
+X['elevation'] = X['elevation'].astype(float)
+
+
+
+pd.get_dummies(X, columns = ['pitch', 'volume'], drop_first= True)
+
+
+layers.experimental.preprocessing.Rescaling(1./256., input_shape=(256, 256, 3))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(264, activation='softmax'))
--- src/spec_cnn.py
+++ src/spec_cnn.py
@@ -54,21 +54,30 @@ test = test.cache().prefetch(buffer_size=AUTOTUNE)
 # model.compile(optimizer='adam',
 #               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
 #               metrics=['accuracy'])
+# model = Sequential()
+# layers.experimental.preprocessing.Rescaling(1./256., input_shape=(256, 256, 3))
+# model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
+# model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(4, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(264, activation='softmax'))
+# #Compile
+# model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
 model = Sequential()
 layers.experimental.preprocessing.Rescaling(1./256., input_shape=(256, 256, 3))
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(4, activation='relu'))
-model.add(Dropout(0.5))
 model.add(Dense(264, activation='softmax'))
-#Compile
-model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-
+print(model.summary())
 epochs=10000
-checkpoint_cb = keras.callbacks.ModelCheckpoint('fourth_model.h5', save_best_only= True)
+checkpoint_cb = keras.callbacks.ModelCheckpoint('fifth_model.h5', save_best_only= True)
 early_stopping_cb = keras.callbacks.EarlyStopping(patience=10)
 tensorboard_cb = keras.callbacks.TensorBoard()
 
