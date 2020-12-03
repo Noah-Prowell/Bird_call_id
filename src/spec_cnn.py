24e149521cf6862112f3b81b5648b0d95cce5b67
Noah Prowell <noahprowell@pop-os.localdomain>
2020-12-02 17:33:25 -0700

    adding seventeen

--- src/spec_cnn.py
+++ src/spec_cnn.py
@@ -70,15 +70,21 @@ test = test.cache().prefetch(buffer_size=AUTOTUNE)
 
 model = Sequential()
 layers.experimental.preprocessing.Rescaling(1./255., input_shape=(128, 128, 3))
-model.add(Conv2D(128, kernel_size=(3, 3),padding = 'same', activation='relu', input_shape=(128, 128, 3)))
+model.add(Conv2D(32, kernel_size=(3, 3),padding = 'same', activation='relu', input_shape=(128, 128, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(256, kernel_size=(3, 3),padding = 'same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, kernel_size=(3, 3),padding = 'same', activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 model.add(Conv2D(64, kernel_size=(3, 3),padding = 'same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, kernel_size=(3, 3),padding = 'same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, kernel_size=(3, 3),padding = 'same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(5, activation='softmax'))
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 print(model.summary())
@@ -87,39 +93,39 @@ checkpoint_cb = keras.callbacks.ModelCheckpoint('seventeen_model.h5', save_best_
 early_stopping_cb = keras.callbacks.EarlyStopping(patience=10)
 # tensorboard_cb = keras.callbacks.TensorBoard()
 
-history1 = model.fit(
-  train,
-  validation_data=test,
-  epochs=250, 
-  batch_size= 28,
-  callbacks=[checkpoint_cb]
-)
-history2 = model.fit(
-  train,
-  validation_data=test,
-  epochs=epochs, 
-  batch_size= 32,
-  callbacks=[checkpoint_cb, early_stopping_cb]
-)
+# history1 = model.fit(
+#   train,
+#   validation_data=test,
+#   epochs=250, 
+#   batch_size= 28,
+#   callbacks=[checkpoint_cb]
+# )
+# history2 = model.fit(
+#   train,
+#   validation_data=test,
+#   epochs=epochs, 
+#   batch_size= 32,
+#   callbacks=[checkpoint_cb, early_stopping_cb]
+# )
 
-acc = history1.history['accuracy']
-val_acc = history1.history['val_accuracy']
+# acc = history1.history['accuracy']
+# val_acc = history1.history['val_accuracy']
 
-loss = history1.history['loss']
-val_loss = history1.history['val_loss']
+# loss = history1.history['loss']
+# val_loss = history1.history['val_loss']
 
-epochs_range = range(250)
+# epochs_range = range(250)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+# plt.figure(figsize=(8, 8))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.savefig('seventeen.png')
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.savefig('seventeen.png')
